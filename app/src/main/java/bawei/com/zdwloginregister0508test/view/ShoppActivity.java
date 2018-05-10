d979fef441f33ae1b49d78c560e9113ffc130345
duminweilizi <58588471@qq.com>
2018-05-10 10:36:36 +0800

    ......

--- app/src/main/java/bawei/com/zdwloginregister0508test/view/ShoppActivity.java
+++ app/src/main/java/bawei/com/zdwloginregister0508test/view/ShoppActivity.java
@@ -3,14 +3,20 @@ package bawei.com.zdwloginregister0508test.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.drawee.view.SimpleDraweeView;
+
+
+
 import bawei.com.zdwloginregister0508test.R;
 
 
 public class ShoppActivity extends AppCompatActivity {
-
+    String url  = "https://ps.ssl.qhimg.com/dm/420_207_/t01232d36b7107a5367.jpg";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopp);
+        SimpleDraweeView simpleDraweeView = findViewById(R.id.simple);
+        simpleDraweeView.setImageURI(url);
     }
 }
